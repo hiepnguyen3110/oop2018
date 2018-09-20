f05224fe1daff98d169b699befdea7402d26e960
hiepnguyen3110 <nguyenhiep.3110.vctb@gmail.com>
2018-09-20 10:46:27 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,9 +3,36 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    String name;
+    String Id;
+    String group;
+    String email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        this.name = name;
+    }
+    public String getId(){
+        return Id;
+    }
+    public String setId( String Id ){
+        this.Id = Id;
+    }
+    public String getGroup(){
+        return group;
+    }
+    public String setGroup(String group){
+        this.group = group;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String setEmail( String email ){
+        this.email = email;
+    }
+    
     /**
      * Constructor 1
      */
@@ -33,6 +60,10 @@ public class Student {
 
     String getInfo() {
         // TODO:
+        System.out.println("Name: " + this.name);
+        System.out.println("Id: " + this.Id);
+        System.out.println("Group: " + this.group);
+        System.out.println("Email: " + this.email);
         return null; // xóa dòng này sau khi cài đặt
     }
 }

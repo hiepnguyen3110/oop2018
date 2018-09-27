e85919dbd93dd72a282311d59ab529c5cdaa4050
hiepnguyen3110 <nguyenhiep.3110.vctb@gmail.com>
2018-09-27 08:27:31 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,22 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if(b == 0) return a;
+        return gcd(b, a%b);
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        int f0 = 0;
+        int f1 = 1;
+        int fn = 1;
+        if(n == 0 || n == 1 ) return n;
+        if(n == 2) return 1;
+        for( int i = 3; i <= n; i++){
+            f0 = f1;
+            f1 = fn;
+            fn = f0 + f1;
+        }
+        return fn;
     }
 }

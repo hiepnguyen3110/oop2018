320948067b5877c543f57cd12b8b07ea6b2688c2
hiepnguyen3110 <nguyenhiep.3110.vctb@gmail.com>
2018-11-22 10:19:43 +0700

    nop bai tuan 11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,49 @@
 package week11;
 
-public class Task1 {
+import java.util.Arrays;
+
+public class Task1{
+
+    /**
+     *
+     * @param <T>
+     * @param a
+     */
+    public static <T extends Comparable<T>> void sapXepNoiBot(T a[]){
+        int n = a.length;
+        for (int i = 0 ; i < n-1 ; i++){
+            for (int j = i+1 ; j < n ; j++){
+                if(a[i].compareTo(a[j]) > 0){
+                    T temp;
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args){
+        Integer[] Iarr = {0, 4, 2, 3, 9, 6};
+        System.out.println("Truoc khi sap xep: " + Arrays.toString(Iarr));
+        sapXepNoiBot(Iarr);
+        System.out.println("Sau khi sap xep: " + Arrays.toString(Iarr));
+        
+        System.out.println("----------------------------------------------");
+        
+        Double dArr[] = {3.05, 4.6, 9.25, 5.45};
+        System.out.println("Truoc khi sap xep: " + Arrays.toString(dArr));
+        sapXepNoiBot(dArr);
+        System.out.println("Sau khi sap xep: " + Arrays.toString(dArr));
+        
+        System.out.println("----------------------------------------------");
+        
+        Character charArr[] = {'e', 'c', 'd', 'a'};
+        System.out.println("Truoc khi sap xep: " + Arrays.toString(charArr));
+        sapXepNoiBot(charArr);
+        System.out.println("Sau khi sap xep: " + Arrays.toString(charArr));
+        
+    }
+    
+    
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,46 @@
 package week11;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Task2 {
+    
+    public static <T extends Comparable<T>> T max(ArrayList<T> a){
+        T max = a.get(0);
+        for(int i = 1; i < a.size(); i++){
+            if(max.compareTo(a.get(i)) < 0){
+                max = a.get(i);
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args){
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(5);
+        a.add(2);
+        a.add(6);
+        a.add(7);
+        a.add(10);
+        a.add(1);
+        
+        System.out.println("max of array: " + max(a));
+        
+        ArrayList<Character> b = new ArrayList<>();
+        b.add('f');
+        b.add('a');
+        b.add('c');
+        b.add('k');
+        b.add('z');
+        b.add('m');
+        
+        System.out.println("max of array: " + max(b));
+        
+        Random random = new Random();
+        ArrayList<Double> c = new ArrayList<>();
+        for(int i = 0; i < 5; i++){
+            c.add(random.nextDouble() * 100);//sinh so ngau nhien
+        }
+        System.out.println("max of array: " + max(c));
+    }
 }
